bc4ee81ac3b54d985bc734ce02f44707ad95ba39
anggiazizah <anggiazizah18@gmail.com>
2022-02-19 14:50:13 +0700

    Toko Online

---
+++ resources/views/pesan/index.blade.php
@@ -0,0 +1,63 @@
+    @extends('layouts.app')
+
+    @section('content')
+    <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                        <a href="{{ url('home') }}" class="btn btn-primary"><i class=" fa fa-arrow-left"></i>Kembali
+                    </a>
+                    </div>
+                    <div class="col-md-12 mt-2">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ $barang->nama_barang }}</li>
+                    </ol>
+                    </nav>
+                    </div>
+                    <div class="col-md-12 mt-1"></div>
+                    <div class="card">
+                    <div class="card-body">
+                    <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{ url('uploads') }}/{{ $barang->gambar }}" class="rounded mx-auto d-block" width="100%" alt="">
+                    </div>
+                    <div class="col-md-6">
+                    <h3>{{ $barang->nama_barang }}</h3>
+                        <table class="table">
+                                     <tbody>
+                                         <tr>
+                                             <td>Harga</td>
+                                             <td>:</td>
+                                             <td>Rp. {{ number_format($barang->harga) }}</td>
+                                         </tr>
+                                         <tr>
+                                            <td>Stok</td>
+                                             <td>:</td>
+                                             <td>{{ number_format($barang->stok) }}</td>
+                                         </tr>
+                                         <tr>
+                                             <td>keterangan</td>
+                                             <td>:</td>
+                                             <td>{{ $barang->keterangan }}</td>
+                                         </tr>
+                                         <tr>
+                                             <td>Jumlah Pesan</td>
+                                             <td>:</td>
+                                             <td>
+                                                 <form method="post" action="{{ url('pesan') }}/{{ $barang->id }}">
+                                                 @csrf
+                                                     <input type="text" name="jumlah_pesan" class="form-control" required="">
+                                                     <button type="submit" class="btn btn-primary mt-2"><i class="fa fa-shopping-cart"></i> Masukkan Keranjang</button>
+                                                </form>
+                                             </td>
+                                         </tr>
+                                         
+
+                                    </tbody>
+                                 </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+     @endsection
